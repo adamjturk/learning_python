58a0b181974db37404b3e1e9ba604c9882acd732
Adam Turk <adamjturk@Adams-MacBook-Pro.local>
2022-05-28 16:53:15 -0600

    added comments

--- commaCode.py
+++ commaCode.py
@@ -1,4 +1,8 @@
+#purpose of function is to receive a list as a parameter, then return a string
+#that has its entries separated by a comma. Last entry has the word 'and' in front
+#of it to signal the end
 def listfunc(param):
+    #uses a counter to tell when the last entry of the list will come up
     counter = 1
     for i in range(len(param)):
         if counter < len(param):
@@ -8,6 +12,7 @@ def listfunc(param):
             print('and ' + param[i])
 
 
+#code that creates a list to start with
 someList = []
 print('Please enter some things you would like to be in the list: ' +
 ' , or press enter to stop')
@@ -17,4 +22,5 @@ while True:
         break
     someList = someList + [entry]
 
+#function call
 listfunc(someList)
